7d50332045723507ada5bc711f1bd92f1ad70e80
sima9z <159206331+sima9z@users.noreply.github.com>
2024-08-27 17:50:20 +0900

    edit: トップページ イチゴをホバー時のイベントの仕様を変更、食べ終わった後にリピートしない、リンクの文字を消す

--- web/components/index/StrawberryLinks.tsx
+++ web/components/index/StrawberryLinks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 
 import HoverableStrawberry from './HoverableStrawberry';
@@ -6,6 +6,8 @@ import HoverableStrawberry from './HoverableStrawberry';
 import { StrawberriesProps } from '../../types/types_index'
 
 const StrawberryLinks: React.FC<StrawberriesProps> = ({ positions, top, initialColor = 'red' }) => {
+  const [hideText, setHideText] = useState<number | null>(null); // 非表示にするリンクのインデックスを管理
+
   const links = [
     { href: "/members/kato", text: "加藤" },
     null,
@@ -14,6 +16,12 @@ const StrawberryLinks: React.FC<StrawberriesProps> = ({ positions, top, initialC
     null,
     { href: "/members/demo", text: "demo" },
   ];
+
+  const handleHoverEnd = (index: number) => {
+    if (hideText === index) {
+      setHideText(null); // ホバーが終了したときにテキストを再度表示
+    }
+  };
 
   return (
     <>
@@ -25,10 +33,12 @@ const StrawberryLinks: React.FC<StrawberriesProps> = ({ positions, top, initialC
           widthPercent={9.5}
           initialIndex={index}
           initialColor={initialColor}
+          onLastImage={() => setHideText(index)} // 最後の画像に到達したらテキストを非表示にする
+          onHoverEnd={() => handleHoverEnd(index)}
         >
           {links[index] && (
             <Link href={links[index]?.href} className="text-black bold" style={{ fontSize: 'clamp(12px, 2vw, 20px)' }}>
-              {links[index]?.text}
+               {hideText !== index && <span>{links[index]?.text}</span>}
             </Link>
           )}
         </HoverableStrawberry>
