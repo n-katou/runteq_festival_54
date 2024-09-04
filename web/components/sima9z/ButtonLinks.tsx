f4a58a7c30f0ee3a4b69527b1a6e942b6889b58c
sima9z <159206331+sima9z@users.noreply.github.com>
2024-09-04 21:31:48 +0900

    edit:sima9z ボタン追加 レイアウト調整

--- web/components/sima9z/ButtonLinks.tsx
+++ web/components/sima9z/ButtonLinks.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Button, Box } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import GroupsIcon from '@mui/icons-material/Groups';
 
 import { ProfileSectionTheme } from './Theme'
 
 const ButtonLinks = () => {
   return (
     <ThemeProvider theme={ProfileSectionTheme}>
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 4 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 4, flexDirection: { xs: 'column', sm: 'row' }, gap: { xs: 2, sm: 0 }, }}>
           <Box
             component="a"
             href="https://github.com/sima9z"
             target="_blank"
             rel="noopener noreferrer"
-            sx={{ textDecoration: 'none', marginRight: 2}}
+            sx={{ textDecoration: 'none', marginRight: { sm: 2, xs: 0 } }}
           >
             <Button variant="contained" color="secondary" sx={{ zIndex: 20 }}>
               github
@@ -25,10 +26,33 @@ const ButtonLinks = () => {
             href="https://x.com/sima9z77407"
             target="_blank"
             rel="noopener noreferrer"
-            sx={{ textDecoration: 'none' }}
+            sx={{ textDecoration: 'none', marginRight: { sm: 2, xs: 0 } }}
           >
             <Button variant="outlined" color="secondary" sx={{ zIndex: 20 }}>
               X
+            </Button>
+          </Box>
+
+          <Box
+            component="a"
+            href="https://school.runteq.jp/social_portfolios/sima9z"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ textDecoration: 'none' }}
+          >
+            <Button 
+              variant="outlined" 
+              sx={{ 
+                zIndex: 20, 
+                color:'white', 
+                borderColor:'white',
+                '&:hover': {
+                  backgroundColor: '#F2A3A2', // ホバー時の背景色を指定
+                  borderColor: '#A3D9A5', // ホバー時のアウトラインの色を指定
+                } 
+              }}
+            >
+             <GroupsIcon />
             </Button>
           </Box>
         </Box>
