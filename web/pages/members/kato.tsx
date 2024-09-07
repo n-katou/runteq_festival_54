4e19148634666e68f9bbcbcd992d2a2937a8b728
n-katou <102382177+n-katou@users.noreply.github.com>
2024-09-07 19:15:00 +0900

    edit:ソーシャルポートフォリオ追加に伴うレスポンシブ対応

--- web/pages/members/kato.tsx
+++ web/pages/members/kato.tsx
@@ -91,8 +91,8 @@ const KatoPage: React.FC = () => {
                   </div>
                   <div className="mt-8 flex flex-col xl:flex-row items-center gap-8">
                     <Social
-                      containerStyles="flex gap-6"
-                      iconStyles="w-12 h-12 border border-green rounded-full flex justify-center items-center text-white text-base bg-green hover:bg-darkGreen hover:text-white transition-all duration-500 text-5xl"
+                      containerStyles="flex gap-2 sm:gap-6 overflow-x-auto"
+                      iconStyles="w-10 h-10 sm:w-12 sm:h-12 border border-green rounded-full flex justify-center items-center text-white text-base bg-green hover:bg-darkGreen hover:text-white transition-all duration-500 text-4xl"
                     />
                   </div>
                 </motion.div>
